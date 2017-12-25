ffeff12060ca0a35fc676e1c414d9f671a152046
Afzal Najam <afzalnaj@gmail.com>
2017-12-25 03:46:04 -0500

    Initial Collapsing AppBar

--- app/src/main/java/com/spacebitlabs/plantly/data/source/UserPlantsStore.kt
+++ app/src/main/java/com/spacebitlabs/plantly/data/source/UserPlantsStore.kt
@@ -9,12 +9,18 @@ class UserPlantsStore {
     val plants: ArrayList<Plant> = ArrayList()
 
     init {
-        plants.add(Plant(0,"Sansa"))
-        plants.add(Plant(1,"Drogo"))
-        plants.add(Plant(2,"Cro"))
-        plants.add(Plant(3,"Krypton"))
-        plants.add(Plant(4,"Xenon"))
-        plants.add(Plant(5,"Argon"))
+        plants.add(Plant(0, "Sansa"))
+        plants.add(Plant(1, "Drogo"))
+        plants.add(Plant(2, "Cro"))
+        plants.add(Plant(3, "Krypton"))
+        plants.add(Plant(4, "Xenon"))
+        plants.add(Plant(5, "Argon"))
+        plants.add(Plant(6, "Sansa"))
+        plants.add(Plant(7, "Drogo"))
+        plants.add(Plant(8, "Cro"))
+        plants.add(Plant(9, "Krypton"))
+        plants.add(Plant(10, "Xenon"))
+        plants.add(Plant(11, "Argon"))
     }
 
     fun savePlant(plant: Plant) {
