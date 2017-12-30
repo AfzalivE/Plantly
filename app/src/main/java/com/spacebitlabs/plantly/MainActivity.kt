8b20777cb98ef87d1718be40e10a2425825b44a8
Afzal Najam <afzalnaj@gmail.com>
2017-12-29 23:14:46 -0500

    Move appbar to fragment_plants

--- app/src/main/java/com/spacebitlabs/plantly/MainActivity.kt
+++ app/src/main/java/com/spacebitlabs/plantly/MainActivity.kt
@@ -3,38 +3,26 @@ package com.spacebitlabs.plantly
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.GridLayoutManager
-import com.spacebitlabs.plantly.data.source.UserPlantsStore
-import com.spacebitlabs.plantly.plants.PlantsAdapter
 import com.spacebitlabs.plantly.plants.PlantsFragment
-import kotlinx.android.synthetic.main.fragment_plants.*
 
 /**
  * Home Activity
  */
 class MainActivity : AppCompatActivity() {
 
-    private val plantAdapter = PlantsAdapter()
-    private val userPlantsStore: UserPlantsStore = UserPlantsStore()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.content_frame)
-//        setFragment(fragment)
-
-        plant_list.layoutManager = GridLayoutManager(this, 2)
-        plant_list.adapter = plantAdapter
-
-        plantAdapter.setPlantList(userPlantsStore.plants)
+        val fragment: Fragment? = supportFragmentManager.findFragmentById(R.id.content_frame)
+        setFragment(fragment)
     }
 
     private fun setFragment(newFragment: Fragment?) {
         val fragment: Fragment = newFragment ?: PlantsFragment.newInstance()
 
-//        supportFragmentManager.beginTransaction()
-//                .replace(R.id.content_frame, fragment)
-//                .commit()
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit()
     }
 }
