46d96092386dcc486403d79007e6ac4a481fdf26
Afzal Najam <afzalnaj@gmail.com>
2021-02-06 14:20:49 -0500

    Make test pass

--- app/src/androidTest/java/com/spacebitlabs/plantly/PlantListTest.kt
+++ app/src/androidTest/java/com/spacebitlabs/plantly/PlantListTest.kt
@@ -7,7 +7,6 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -33,7 +32,7 @@ class PlantListTest {
     class AddPlantRobot {
         class Assertions {
             fun addPlantIsShown() {
-                onView(withId(R.id.save)).check(matches(not(ViewMatchers.isDisplayed())))
+                onView(withId(R.id.save)).check(matches(ViewMatchers.isDisplayed()))
             }
         }
     }
